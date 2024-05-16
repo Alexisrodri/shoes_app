c837e32fa0ccb3c41d012d4e8468a26eb16b9131
Alex Avila <niteravilaalexis@gmail.com>
2024-05-15 19:13:26 -0500

    CustomAppbar y imgs agregadas

---
+++ lib/src/pages/shoe_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_app/src/widgets/widgets.dart';
+
+class ShoePage extends StatelessWidget {
+   
+  const ShoePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      body:CustomAppbar(text: 'For you',),
+    );
+  }
+}
